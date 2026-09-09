0bdca547a32f2fc5a2f77babbbf4a1bf85b29e65
nationwide2026 <nationwidedispatching@outlook.com>
2026-09-09 05:03:26 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,8 @@
-apiKey: "AIzaSyA....",
-authDomain: "nationwide-dispatching-7b346.firebaseapp.com",
-projectId: "nationwide-dispatching-7b346",
-storageBucket: "nationwide-dispatching-7b346.appspot.com",
-messagingSenderId: "1234567890",
-appId: "1:1234567890:web:abcdef123456"
+export const firebaseConfig = {
+  apiKey: "AIzaSyXXXXXXX",
+  authDomain: "nationwide-dispatching-7b346.firebaseapp.com",
+  projectId: "nationwide-dispatching-7b346",
+  storageBucket: "nationwide-dispatching-7b346.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdef123456"
+};
