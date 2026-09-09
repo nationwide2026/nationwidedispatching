5e38087b8f0fa7c0e9a26fe1b09c2f67a2ced417
nationwide2026 <nationwidedispatching@outlook.com>
2026-09-09 03:20:06 -0400

    Update firebase-config.js
    const firebaseConfig = {
      apiKey: "...",
      authDomain: "...",
      projectId: "nationwide-dispatching-7b346",
      storageBucket: "nationwide-dispatching-7b346.firebasestorage.app",
      messagingSenderId: "...",
      appId: "..."
    };

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,8 @@
-// Firebase configuration for Nationwide Dispatching
 const firebaseConfig = {
-  apiKey: "AIzaSyDXNZRDMmUJXoqMsFwluElbrSICN65Kmvc",
-  authDomain: "nationwide-dispatching-7b346.firebaseapp.com",
+  apiKey: "...",
+  authDomain: "...",
   projectId: "nationwide-dispatching-7b346",
   storageBucket: "nationwide-dispatching-7b346.firebasestorage.app",
-  messagingSenderId: "280010805970",
-  appId: "1:280010805970:web:42989433f02939aca3930b",
-  measurementId: "G-6KD4M9JMGF"
+  messagingSenderId: "...",
+  appId: "..."
 };
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
