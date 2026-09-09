cf4d6d855244eb05390194cc00688cd81170ade4
nationwide2026 <nationwidedispatching@outlook.com>
2026-09-09 03:39:04 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,1 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyA3FJ8K9EXAMPLEKEY",
-  authDomain: "nationwide-dispatching-7b346.firebaseapp.com",
-  projectId: "nationwide-dispatching-7b346",
-  storageBucket: "nationwide-dispatching-7b346.firebasestorage.app",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdef123456"
-};
+
